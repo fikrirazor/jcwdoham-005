24452571e2f6795447e816d585b6dbf29fa3dd7b
fikrirazor <fikrirazor@outlook.co.id>
2025-11-07 22:12:11 +0700

    feat: add area of rectangle

---
+++ area_persegi_empat/index.ts
@@ -0,0 +1,14 @@
+// Mencari Luas Persegi
+// Inisialisasi 
+// panjang
+// lebar
+// luas = panjang * lebar
+// console.log(luas)
+let lengthRectangle:number = 5;
+let widthRectangle:number = 3;
+
+let area:number;
+
+area = lengthRectangle * widthRectangle;
+
+console.log("luas dari persegi adalah:", area);
