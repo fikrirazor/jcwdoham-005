07f032fc65573f351ce1ae0174dd5bab51c3477e
fikrirazor <fikrirazor@outlook.co.id>
2025-11-07 22:38:22 +0700

    feat:add perimeter of rectangle & change name persegi folder to persegi panjang

---
+++ perimeter_of_rectangle/index.ts
@@ -0,0 +1,13 @@
+// Mencari Keliling persegi
+// input: lenght = 5, width = 3
+// proces: perimeter = 2 x (5+3) = 2 x 8
+// output: 16
+
+let lengthR:number = 5;
+let widthR:number = 3;
+
+let perimeter:number;
+
+perimeter = 2 * (lengthR+widthR);
+
+console.log("Perimeter of rectangle is:", perimeter)
