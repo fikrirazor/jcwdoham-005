a8608f5481b68df8f167953033ebb255b88e0b55
fikrirazor <fikrirazor@outlook.co.id>
2025-11-10 07:56:20 +0700

    feat: refactor BMI calculation into a separate function

--- d4_array_and_function/exercise3_BMICalculator/index.ts
+++ d4_array_and_function/exercise3_BMICalculator/index.ts
@@ -1,18 +1,22 @@
 // BMI = BMI = weight (kg) / (height (meter))**2
 
+function BMICalculator(weight, height){
+    let BMI:number = weight / (height**2);
+
+    if(BMI <= 18.5){
+        console.log("less weight");
+    } else if (BMI <= 24.9){
+        console.log("ideal");
+    } else if (BMI <= 29.9){
+        console.log("overweight");
+    } else if (BMI <= 39.9){
+        console.log("very overweight");
+    } else {
+        console.log("obesity")
+    }
+}
+
 let weight:number = 20; // kg
 let height:number = 158; //meters
 
-let BMI:number = weight / (height**2);
-
-if(BMI <= 18.5){
-    console.log("less weight");
-} else if (BMI <= 24.9){
-    console.log("ideal");
-} else if (BMI <= 29.9){
-    console.log("overweight");
-} else if (BMI <= 39.9){
-    console.log("very overweight");
-} else {
-    console.log("obesity")
-}
+BMICalculator(weight,height);
