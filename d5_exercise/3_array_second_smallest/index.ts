57742ec9452cf5bcbb955792d2e3b81ce7577de8
fikrirazor <fikrirazor@outlook.co.id>
2025-11-11 15:48:52 +0700

    fix: Correct logic in secondSmallestFinder function to properly find the second smallest number

--- d5_exercise/3_array_second_smallest/index.ts
+++ d5_exercise/3_array_second_smallest/index.ts
@@ -1,7 +1,18 @@
-let numbers = [5, 3, 1, 7, 2, 6]
-numbers.sort();
-for(let i = 0; i<=numbers.length;i++){
-    if(i==1){
-        console.log(numbers[i])
+// Tujuan Mencari angka ke 2 paling kecil di dalam Array
+// input : n = [5, 3, 1, 7, 2, 6]
+// output : 2
+
+function secondSmallestFinder(arrayNumber: Array<number>) {
+  numbers.sort();
+  let theSmallest: number;
+  for (let i = 0; i <= numbers.length; i++) {
+    if (i == 1) {
+      theSmallest = numbers[i];
     }
+  }
+  return theSmallest;
 }
+
+let numbers = [5, 3, 1, 7, 2, 6];
+
+console.log(`the smallest number from Array [${numbers}] is ${secondSmallestFinder(numbers)}`);
