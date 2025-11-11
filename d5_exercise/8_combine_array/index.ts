fa6989d700fdc7932ec876061568a28089c00597
fikrirazor <fikrirazor@outlook.co.id>
2025-11-11 20:36:02 +0700

    feat(d5_exercise): Implement combineTwoArray function to merge two arrays

---
+++ d5_exercise/8_combine_array/index.ts
@@ -0,0 +1,9 @@
+function combineTwoArray(arrA,arrB){
+    let arrC = arrA.concat(arrB);
+    return arrC
+}
+
+arrA = [1,2,3];
+arrB = [4,5,6];
+
+console.log(combineTwoArray(arrA,arrB));
---
+++ d5_exercise/9_find_duplicate_array/index.ts
@@ -0,0 +1,16 @@
+/*
+Write a function to find duplicate values in an array
+a. Example : arr = [1, 2, 2, 2, 3, 3, 4, 5, 5] → [2, 3, 5]
+*/
+
+function find_duplicate_array(){
+    let arr_duplicate = [];
+
+
+}
+
+let arr = [1, 2, 2, 2, 3, 3, 4, 5, 5];
+
+const output = find_duplicate_array(arr);
+
+console.log(output);
