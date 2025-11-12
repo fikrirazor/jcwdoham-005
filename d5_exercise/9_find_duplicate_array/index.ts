8926d2a0ffdc5503f6d9953ab71ca20aebe0fef4
fikrirazor <fikrirazor@outlook.co.id>
2025-11-12 08:24:42 +0700

    feat(find_duplicate_array): implement function to find duplicate

--- d5_exercise/9_find_duplicate_array/index.ts
+++ d5_exercise/9_find_duplicate_array/index.ts
@@ -3,10 +3,16 @@ Write a function to find duplicate values in an array
 a. Example : arr = [1, 2, 2, 2, 3, 3, 4, 5, 5] → [2, 3, 5]
 */
 
-function find_duplicate_array(){
+function find_duplicate_array(arr){
     let arr_duplicate = [];
-
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr_duplicate.push(arr[i]);
+            }
+        }
+    }
+    return arr_duplicate;
 }
 
 let arr = [1, 2, 2, 2, 3, 3, 4, 5, 5];
