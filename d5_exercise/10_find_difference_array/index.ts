f1c44156411c86c1111d601d7cab525fa448bb0d
fikrirazor <fikrirazor@outlook.co.id>
2025-11-12 08:47:48 +0700

    refactor: update data types

--- d5_exercise/10_find_difference_array/index.ts
+++ d5_exercise/10_find_difference_array/index.ts
@@ -2,8 +2,8 @@ function find_difference(arr1,arr2){
 
 }
 
-arr1 = [1, 2, 3, 4, 5];
-arr2 = [3, 4, 5, 6, 7];
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [3, 4, 5, 6, 7];
 let arr_difference = [];
 let l = arr1.length;
 // find difference in two array
